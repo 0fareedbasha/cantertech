2c7e96f22f5456ece1f12a87427ffa049ecf7bb6
0fareedbasha <142483789+0fareedbasha@users.noreply.github.com>
2023-08-17 14:07:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import streamlit_analytics
 
-
-with streamlite_analytics.track():
+with streamlit_analytics.track():
     st.text_input("write your name")
     st.selectbox("select your favorite",["guvi","data","science"])
     st.button("click me")
